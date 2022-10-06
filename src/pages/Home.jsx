5603c4265579bb373be233636794f79db1cb1f2e
Titus Jusu Nabieu <34219215+TitusJusuNabieu@users.noreply.github.com>
2022-10-06 13:19:38 +0000

    My initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Nav from '../components/Home/Nav'
+import Hero from '../components/Home/Hero'
+import Service from '../components/Home/Service'
+import Footer from '../components/Home/Footer'
+
+function Home() {
+  return (
+    <>
+    <Nav/>
+    <Hero/>
+    <Service/>
+    <Footer/>
+    </>
+    
+  )
+}
+
+export default Home
